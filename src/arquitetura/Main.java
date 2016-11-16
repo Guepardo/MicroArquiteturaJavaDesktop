7ac385f09fe26306e66245b97104adb146fd4dcb
guepardo <bsinet@hotmail.com>
2016-11-15 23:39:02 -0200

    plus: um pequeno tutorial na classe com o método main

--- src/arquitetura/Main.java
+++ src/arquitetura/Main.java
@@ -22,11 +22,31 @@ public class Main {
      */
     public static void main(String[] args) throws SQLException {
         // TODO code application logic here
-      ResultSet set = DB.exec("SELECT COUNT(*) AS qtd FROM site"); 
+        Site s = new Site(); 
+        //Select * from site where id = 1; Site.findById(int)
+        System.out.println(s.findById(1).getLabel()); 
+        
+        //Select * from site; Site.all()
+        for( Site temp: s.all()){
+            System.out.println(temp.getId()+" "+ temp.getUrl());
+        }
+        
+        Site a = new Site();
+        
+        a.findById(3); 
+        a.setLabel("Label Atualizado");
+        
+        //Gera query Update de acordo com o contexto do objeto
+        a.update(); 
+        
+        
+        //Executa qualquer query e retorna um ResultSet.
+         ResultSet set = DB.exec("SELECT COUNT(*) AS qtd FROM site"); 
       
-      while(set.next()){
-          System.out.println(set.getString("qtd"));
-      }
+        while(set.next()){
+            System.out.println(set.getString("qtd"));
+        }
+    
     }
     
 }
